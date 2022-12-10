fa759229a9bfd8d289c51a27b657057bb647e2a3
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-10 12:23:18 +0300

    2 files modified

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import './style.scss'
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Custom Javascript Slider</h1>`;
