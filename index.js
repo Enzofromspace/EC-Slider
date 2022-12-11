cd9143f2b75ef77633e7e353a016019b3e139f94
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-11 12:58:08 +0300

    Saving new slider function and variables before I break it

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ import './style.scss'
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Custom Javascript Slider</h1>`;
+
+function Slider(slider){
+  if(!(slider instanceof Element)) {
+    throw new Error('No Slider avail');
+  }
+  // create variables for working with the slider 
+  //variable will help keep track of the sliders position - whats current, next or prev
+  let current;
+  let prev;
+  let next;
+  
+  const slides = slider.querySelector('.slides');
+}
+
+const mySlider = Slider(document.querySelector('.slider'));
+const dogSlider = Slider(document.querySelector('.dog-slider'))  
